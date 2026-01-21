8dc51dd040a681be7971c6c66ba212dd25b786ec
wahyuprasettya <wahyu.prasettya@gmail.com>
2026-01-21 11:12:21 +0700

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a + b;
+}
+
+module.exports = { tambah, kurang };
---
+++ test.js
@@ -0,0 +1,9 @@
+const { tambah, kurang } = require('./index');
+
+test('2 + 3 harus 5', () => {
+  expect(tambah(2, 3)).toBe(5);
+});
+
+test('5 - 3 harus 2', () => {
+  expect(kurang(5, 3)).toBe(2);
+});
